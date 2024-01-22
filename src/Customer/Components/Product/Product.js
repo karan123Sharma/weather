06478bf5fd8433a2750ab2f8045d2cc3c9741ae9
Adityatathod141 <adityatathod333@gmail.com>
2024-01-22 18:47:12 +0530

    commit

--- src/Customer/Components/Product/Product.js
+++ src/Customer/Components/Product/Product.js
@@ -38,7 +38,7 @@ export default function Product() {
     const searchParms = new URLSearchParams(location.search);
     let filterValue = searchParms.getAll(sectionId);
     if (filterValue.length > 0 && filterValue[0].split(",").includes(value)) {
-      filterValue = filterValue[0].split(",").filter((item) => item !== value);
+      filterValue = filterValue[0].split(",").filter((item) => item != value);
       if (filterValue.length === 0) {
         searchParms.delete(sectionId);
       }
